265b5330ab93b12a041674275598188b96cb3f39
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2023-05-16 22:02:44 +0200

    Update main.cpp

--- compiler/source/main.cpp
+++ compiler/source/main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char** argv)
 {
 	// input and output files
 	std::filesystem::path in; // = "C:/Users/vitod/Desktop/vdtproto/test/test1.h";
+	std::vector<std::filesystem::path> includes;
 	bool recursive = true;
 
 	if (argc == 0)
@@ -22,6 +23,7 @@ int main(int argc, char** argv)
 		return COMPILER_ERROR;
 	}
 
+	bool includeFiles = false;
 	for (int i = 1; i < argc; ++i)
 	{
 		if (i == 1)
@@ -32,10 +34,28 @@ int main(int argc, char** argv)
 		{
 			if (argv[i] == std::string("-no-recursion"))
 			{
+				includeFiles = false;
 				recursive = false;
+			}
+			else if (argv[i] == std::string("-include"))
+			{
+				includeFiles = true;
+			}
+			else if (includeFiles)
+			{
+				includes.push_back(argv[i]);
 			}
 		}
 	}
+
+	std::cout << "input: " << in.string() << std::endl;
+	std::cout << "recursion: " << (recursive ? "true" : "false") << std::endl;
+	std::cout << "includes: " << std::endl;
+	for (const std::filesystem::path& file : includes)
+	{
+		std::cout << "- "  << file.string() << std::endl;
+	}
+	std::cout << std::endl;
 
 	if (!std::filesystem::exists(in))
 	{
@@ -43,11 +63,38 @@ int main(int argc, char** argv)
 		return COMPILER_ERROR;
 	}
 
+	FileCollection fileCollection;
+	TypeCollection typeCollection;
+	SymbolTable symbolTable;
+
+	// includes
+	for (const std::filesystem::path& file : includes)
+	{
+		if (std::filesystem::is_directory(file))
+		{
+			for (const std::filesystem::path& _file : Directory::scan(file, { ".h" }, recursive))
+			{
+				SymbolList symbolList;
+				if (!Parser::parse(typeCollection, symbolTable, symbolList, _file))
+				{
+					std::cout << "Failed to parse the input file " << _file << std::endl;
+					return COMPILER_ERROR;
+				}
+			}
+		}
+		else
+		{
+			SymbolList symbolList;
+			if (!Parser::parse(typeCollection, symbolTable, symbolList, file))
+			{
+				std::cout << "Failed to parse the input file" << std::endl;
+				return COMPILER_ERROR;
+			}
+		}
+	}
+
 	if (std::filesystem::is_directory(in))
 	{
-		FileCollection fileCollection;
-		TypeCollection typeCollection;
-		SymbolTable symbolTable;
 		for (const std::filesystem::path& file : Directory::scan(in, { ".h" }, recursive))
 		{
 			SymbolList symbolList;
@@ -60,26 +107,9 @@ int main(int argc, char** argv)
 
 			fileCollection.insert(std::make_pair(file, symbolList));
 		}
-
-		if (fileCollection.empty())
-		{
-			// Nothing to generate
-			return COMPILER_SUCCESS;
-		}
-
-		for (auto& [file, symbols] : fileCollection)
-		{
-			if (!Encoder::encode(symbols, typeCollection, symbolTable, file.parent_path(), file.filename().string()))
-			{
-				std::cout << "Failed to encode the generated code for the file " << file.filename().string() << std::endl;
-				return COMPILER_ERROR;
-			}
-		}
 	}
 	else
 	{
-		TypeCollection typeCollection;
-		SymbolTable symbolTable;
 		SymbolList symbolList;
 		if (!Parser::parse(typeCollection, symbolTable, symbolList, in))
 		{
@@ -87,15 +117,20 @@ int main(int argc, char** argv)
 			return COMPILER_ERROR;
 		}
 
-		if (symbolList.empty())
-		{
-			// Nothing to generate
-			return COMPILER_SUCCESS;
-		}
+		fileCollection.insert(std::make_pair(in, symbolList));
+	}	
 
-		if (!Encoder::encode(symbolList, typeCollection, symbolTable, in.parent_path(), in.filename().string()))
+	if (fileCollection.empty())
+	{
+		// Nothing to generate
+		return COMPILER_SUCCESS;
+	}
+
+	for (auto& [file, symbols] : fileCollection)
+	{
+		if (!Encoder::encode(symbols, typeCollection, symbolTable, file.parent_path(), file.filename().string()))
 		{
-			std::cout << "Failed to encode the generated code for the file " << in.filename().string() << std::endl;
+			std::cout << "Failed to encode the generated code for the file " << file.filename().string() << std::endl;
 			return COMPILER_ERROR;
 		}
 	}
